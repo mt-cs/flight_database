067110d4ef9515eb9c055b1a9b9d47e66be44e21
mt-cs <Mtz31891usfca19>
2021-03-22 17:57:08 -0700

    TestMain added print

--- src/main/java/skipList/TestMain.java
+++ src/main/java/skipList/TestMain.java
@@ -8,17 +8,18 @@ public class TestMain {
 
     public static void main(String[] args) {
         /* FLIGHT KEY */
+        System.out.println("RUN FLIGHT KEY");
         FlightKey fk1 = new FlightKey("FRA", "JFK", "06/05/2020", "07:00");
-        System.out.println(fk1.toString());
-        System.out.println(fk1.getDate());
-        System.out.println(fk1.getTime());
+        System.out.println("fk1 toString: " + fk1.toString());
+        System.out.println("Date: " +fk1.getDate());
+        System.out.println("Time: " +fk1.getTime());
         FlightKey fk2 = new FlightKey("FRA", "JFK", "05/03/2021", "10:00");
-        System.out.println(fk2.toString());
-        System.out.println(fk1.compareTo(fk2));
-
+        System.out.println("fk2 toString: " + fk2.toString());
+        System.out.println("fk1 compareTo fk2: " + fk1.compareTo(fk2));
         FlightKey fk3 = new FlightKey(fk2);
-        System.out.println(fk3.toString());
-        System.out.println(fk2.compareTo(fk3));
+        System.out.println("fk3 toString: " + fk3.toString());
+        System.out.println("fk2 compareTo fk3: " + fk2.compareTo(fk3));
+
         FlightKey key1 = new FlightKey("SFO", "JFK", "03/15/2021", "08:00");
         FlightKey key2 = new FlightKey("SFO", "JFK", "03/15/2021", "08:15");
         FlightKey key3 = new FlightKey("SFO", "JFK", "03/15/2021", "09:00");
@@ -29,26 +30,35 @@ public class TestMain {
         FlightKey key8 = new FlightKey("SFO", "JFK", "03/15/2021", "07:15");
 
         /* FLIGHT DATA */
+        System.out.println("\nRUN FLIGHT DATA");
         FlightData data1 = new FlightData("LH122", 300.0);
         FlightData data2 = new FlightData("LH150", 500.0);
-        System.out.println(data1.getFlightNumber() + " " + data1.getPrice());
+        System.out.println("Data1: " + data1.getFlightNumber() + " " + data1.getPrice());
 
         /* FLIGHT NODE */
+        System.out.println("\nRUN FLIGHT NODE");
         FlightNode node1 = new FlightNode(fk1, data1);
         FlightNode node2 = new FlightNode(node1);
-        System.out.println(node2.getKey());
-        System.out.println(node2.getData().getFlightNumber() + " " + node2.getData().getPrice());
+        System.out.println("Node2 key before: " + node2.getKey());
+        System.out.println("Node2 data before: " + node2.getData().getFlightNumber() + " " + node2.getData().getPrice());
+        node2.setKey(key1);
+        node2.setData(data2);
+        System.out.println("Node2 key after: " + node2.getKey());
+        System.out.println("Node2 data after: " + node2.getData().getFlightNumber() + " " + node2.getData().getPrice());
         FlightNode node3 = new FlightNode(fk2, data2);
         node2.setNext(node3);
 
         /* FLIGHT LIST*/
         FlightList flight1 = new FlightList();
         FlightKey zzz_key =  new FlightKey("zzz", "zzz", "12/31/9999", "24:00");
+        System.out.println("\nTest find and insert, all should print true:");
         System.out.println(flight1.find(zzz_key));
         System.out.println(flight1.insert(fk1, data1));
         System.out.println(flight1.insert(fk2, data2));
         System.out.println(flight1.find(fk1));
+        System.out.println("\nFlight1 toString:");
         System.out.println(flight1.toString());
+        System.out.println("\nPrinting to file");
         flight1.print("output.txt");
 
         FlightList flight2 = new FlightList();
@@ -64,19 +74,19 @@ public class TestMain {
 
         System.out.println(flight2.toString());
         System.out.println("\nSUCCESSORS");
-        System.out.println("Key: " + key1.toString());
-        List<FlightNode> arr = flight2.successors(key1);
+        System.out.println("Key: " + key6.toString());
+        List<FlightNode> arr = flight2.successors(key6);
         for (int i = 0; i < arr.size(); i++) {
             System.out.println(arr.get(i).getKey());
         }
         System.out.println("\nPREDECESSORS");
-        System.out.println("Key: " + key1.toString());
-        arr = flight2.predecessors(key1, 15);
+        System.out.println("Key: " + key6.toString());
+        arr = flight2.predecessors(key6, 15);
         for (int i = 0; i < arr.size(); i++) {
             System.out.println(arr.get(i).getKey());
         }
         System.out.println("\nFIND FLIGHTS");
-        arr = flight2.findFlights(key1, 2);
+        arr = flight2.findFlights(key6, 2);
         for (int i = 0; i < arr.size(); i++) {
             System.out.println(arr.get(i).getKey());
         }
